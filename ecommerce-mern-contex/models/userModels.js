0e9e3ecc49cfdc951bc31d8877d058045cf70065
manojsinghchouhan00 <manojsinghchouhan00@gmail.com>
2023-11-21 16:32:52 +0530

    jwt token implement successfull

--- ecommerce-mern-contex/models/userModels.js
+++ ecommerce-mern-contex/models/userModels.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
         type :String,
         require : true,
     },
+    password :{
+        type :String,
+        require : true,
+    },
     role :{
          type :Number,
         default : 0,
--- ecommerce-mern-contex/routes/authRoute.js
+++ ecommerce-mern-contex/routes/authRoute.js
@@ -1,10 +1,13 @@
 import express from "express";
-import {registerController} from "../controller/authController.js"
+import {registerController, loginController} from "../controller/authController.js"
 
 // route object
 const router = express.Router();
 
 // REGISTER || METHOD
 router.post('/register',registerController);
+
+// LOHIN || POST
+router.post('/login',loginController)
 
 export default router;
