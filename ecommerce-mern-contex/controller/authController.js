a3552355744992bacce7340d9808a40a54563ce5
manojsinghchouhan00 <manojsinghchouhan00@gmail.com>
2023-11-21 15:00:27 +0530

    first user created /work on authController.js

--- ecommerce-mern-contex/controller/authController.js
+++ ecommerce-mern-contex/controller/authController.js
@@ -1,2 +1,50 @@
-export const registerController = () =>{};
+import userModel from "../models/userModels.js";
+import { hashPassword } from "../helpers/authHelper.js";
+
+export const registerController =async (req,res) =>{
+    try {
+        const {name,email,phone,password,address} = req.body;
+        if(!name){
+            return res.send({error : "Name is Required"})
+        }
+        if(!email){
+            return res.send({error : "Email is Required"})
+        }
+        if(!phone){
+            return res.send({error : "Phone is Required"})
+        }
+        if(!password){
+            return res.send({error : "Password is Required"})
+        }
+        if(!address){
+            return res.send({error : "Address is Required"})
+        }
+
+        // existing user
+        const existingUser =  await userModel.findOne({email})
+        if(existingUser){
+            res.status(200).send({
+                success:false,
+                message :'Already Resister please login'
+            })
+        }
+        //register user
+        const hashedPassword = await hashPassword(password)
+
+        // save
+        const user =await new userModel({name,email,phone,address,password:hashedPassword}).save()
+        res.status(201).send({
+            success:true,
+            message : "user register successfully",
+            user
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message:"Error in registration",
+            error
+        })
+    }
+};
 
--- ecommerce-mern-contex/server.js
+++ ecommerce-mern-contex/server.js
@@ -3,7 +3,7 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
-import authRout from "./routes/authRoute.js"
+import authRouter from "./routes/authRoute.js"
 // config env
  dotenv.config();
 
@@ -17,7 +17,7 @@ const app = express();
  app.use(morgan('dev'))
 
  // route
- app.use('/api/v1/auth', authRout);
+ app.use('/api/v1/auth', authRouter);
 
 
 //  rest api
