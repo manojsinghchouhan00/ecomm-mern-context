f0b4043e1bec97f2d8931d48fee2c7bb7317c654
manojsinghchouhan00 <manojsinghchouhan00@gmail.com>
2023-11-21 17:30:54 +0530

    implement middle ware admin and requiresugn in

--- ecommerce-mern-contex/controller/authController.js
+++ ecommerce-mern-contex/controller/authController.js
@@ -77,7 +77,7 @@ export const loginController =async (req,res)=>{
             })
         }
         // token
-        const token = await JWT.sign({_id : user._id}, process.env.JWT_SECRET,{expiresIn:'7d'});
+        const token =  JWT.sign({_id : user._id}, process.env.JWT_SECRET,{expiresIn:'7d'});
         res.status(200).send({
             success:true,
             message:"login successfully",
@@ -98,3 +98,9 @@ export const loginController =async (req,res)=>{
         })
     }
 };
+
+// testController
+export const testController = (req,res) =>{
+    console.log("protected route");
+    res.send("protected route")
+}
--- ecommerce-mern-contex/routes/authRoute.js
+++ ecommerce-mern-contex/routes/authRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
-import {registerController, loginController} from "../controller/authController.js"
+import {registerController, loginController, testController} from "../controller/authController.js"
+import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
 
 // route object
 const router = express.Router();
@@ -9,5 +10,8 @@ router.post('/register',registerController);
 
 // LOHIN || POST
 router.post('/login',loginController)
+
+// test route
+router.get('/test', requireSignIn ,isAdmin, testController)
 
 export default router;
